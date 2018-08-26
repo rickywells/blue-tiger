863548d015ba123f6b37003e5df5d97f75d3150b
rickywells <ricky.wells@gmail.com>
2018-08-26 16:02:05 -0400

    armory test

--- BlueTiger/src/TestClass.java
+++ BlueTiger/src/TestClass.java
@@ -1,9 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestClass 
 {
-
+	public static void main(String[] args) {
+		TestClass test = new TestClass();
+	}
+	
 	public TestClass()
 	{
-		System.out.println("Hello, World");
+		Weapon sword = 
+				new Weapon("Longsword", 6, 3, 100, false, false, 0, false, 
+						true, false, false, false);
+		Weapon spear = 
+				new Weapon("Spear", 4, 3, 100, false, false, 0, true, 
+						false, false, true, false);
+		Weapon dagger = 
+				new Weapon("Dagger", 4, 2, 100, true, false, 0, false, 
+						false, false, false, true);
+		
+		List<Weapon> armory = new ArrayList<Weapon>();
+		armory.add(spear);
+		armory.add(sword);
+		armory.add(dagger);
+		
+		for (int i = 0; i < armory.size(); i++)
+		{
+			System.out.println(armory.get(i).getName());
+			System.out.println("d"+ armory.get(i).getDamage());
+			System.out.println("Speed: " + armory.get(i).getSpeed());
+			if(armory.get(i).isDualWield()) {System.out.println("Can dual wield");}
+			if(armory.get(i).isMajorWounding()) {System.out.println("Deals Major Wounds");}
+			if(armory.get(i).isRanged()) {System.out.println("Range: " + armory.get(i).getRange() + "'");}
+			if(armory.get(i).isStealthy()) {System.out.println("Concealable");}
+			if(armory.get(i).isStunning()) {System.out.println("Can stun");}
+			if(armory.get(i).isThrusting()) {System.out.println("Can thrust at opponent out of reach");}
+			if(armory.get(i).isTwoHanded()) {System.out.println("Requires two hands");}
+			System.out.println("----------------------------------------");
+		}
 	}
 }
