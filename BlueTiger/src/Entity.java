7ad25bf3840efbc12927ca232cdd94b7cd921afa
rickywells <ricky.wells@gmail.com>
2020-04-04 16:10:28 -0400

    update

---
+++ BlueTiger/src/Entity.java
@@ -0,0 +1,26 @@
+
+public class Entity {
+	private String name;
+	private int maxHealth;
+	private int currentHealth;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+}
